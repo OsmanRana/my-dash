f5dc87c13b789678e93a868715e3eda1a83012e1
OsmanRana <osmancmc@yahoo.com>
2022-05-20 21:48:39 +0600

    initial commit

---
+++ src/components/RegisterPage/RegisterPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RegisterForm from './RegisterForm/RegisterForm';
+import RegisterImage from './RegisterImage/RegisterImage';
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <RegisterImage/>
+            <RegisterForm/>
+        </div>
+    );
+};
+
+export default RegisterPage;
