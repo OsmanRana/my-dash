2af06f07ca2f4205c3f00694ff1cbf9330495772
OsmanRana <osmancmc@yahoo.com>
2022-05-20 22:13:23 +0600

    input field added

--- src/components/RegisterPage/RegisterForm/RegisterForm.js
+++ src/components/RegisterPage/RegisterForm/RegisterForm.js
@@ -4,6 +4,31 @@ const RegisterForm = () => {
   return (
     <div>
       <h3>Create an account</h3>
+      <form>
+        <label>Your email address</label>
+        <br />
+        <input />
+        <br />
+        <label>Your password</label>
+        <br />
+        <input />
+        <br />
+        <label>Confirm your password</label>
+        <br />
+        <input />
+        <br />
+        <label>Your full name</label>
+        <br />
+        <input />
+        <br />
+        <label>Your phone number</label>
+        <br />
+        <input />
+        <br />
+
+        <input type="checkbox" />
+        <label>I read and agree Terms and Conditions</label>
+      </form>
     </div>
   );
 };
